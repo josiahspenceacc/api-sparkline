bd04a5b64f3a01f6a2d936df0f8edbc40984fc9c
josiahspenceacc <45018435+josiahspenceacc@users.noreply.github.com>
2023-12-20 14:26:56 -0600

    Update sparkline.js

--- functions/sparkline.js
+++ functions/sparkline.js
@@ -9,7 +9,7 @@ function sparkline(values, width, height, color) {
 
   // TODO handle lines at edge getting cut off for larger stroke widths
   line.setStrokeWidth(2);
-  line.setFill("#dddddd55");
+  line.setFill("#cccccc55");
 
   if(color) {
     line.setStroke(color || "#000000");
